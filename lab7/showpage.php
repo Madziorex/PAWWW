ed5044f4d33f210da46630ec09624ce9525e3e2c
Madziorex <92018274+Madziorex@users.noreply.github.com>
2023-11-29 15:16:40 +0100

    lab7

---
+++ lab7/showpage.php
@@ -0,0 +1,36 @@
+<?php
+function PokazPodstrone($id)
+{
+    $id_clear = htmlspecialchars($id);
+
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbname = 'moja_strona';
+    $link = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+    if (!$link) {
+        die('Nie udało się połączyć z bazą danych: ' . mysqli_connect_error());
+    }
+
+    $query = "SELECT * FROM page_list WHERE id = ? LIMIT 1";
+    $stmt = mysqli_prepare($link, $query);
+    mysqli_stmt_bind_param($stmt, 's', $id_clear);
+    mysqli_stmt_execute($stmt);
+
+    $result = mysqli_stmt_get_result($stmt);
+    $row = mysqli_fetch_array($result);
+
+    if(empty($row['id']))
+    {
+        $web = '[nie_znaleziono_strony]';
+    }
+    else
+    {
+        $web = $row['page_content'];
+    }
+
+    mysqli_close($link);
+    return $web;
+}
+?>
